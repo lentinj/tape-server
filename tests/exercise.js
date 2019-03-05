cc7daffe43ca6250d66a86051f8ac3ce4ec27898
Jamie Lentin <jm@lentin.co.uk>
2019-03-05 10:44:09 +0000

    console: Exercise other options

--- tests/exercise.js
+++ tests/exercise.js
@@ -5,5 +5,18 @@ console.log("Hello");
 console.log("The following value is undefined:");
 console.log(undefined);
 
+console.log("The following value is an HTML element:");
+console.log(new window.Option("Inner Text", "val"));
+
 console.log("Arrays:");
 console.log(["1 potato", "2 potato", "3 potato", undefined, 5]);
+
+console.log("Objects:");
+console.log({a: "1 potato", b: 33});
+
+console.error("error");
+console.warn("warn");
+console.dir("dir");
+console.debug("debug");
+console.info("info");
+console.trace("trace");
